3904fdfe60cb09eafcd47490d5ae202f8329a9e6
Alexandr Kruglov <alexandr.gpro@gmail.com>
2023-12-04 22:09:32 +0300

    Initial commit

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ class Student:
         self.grades = {}
 
     def rate_Lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+        if isinstance((lecturer, Lecturer)
+                      and course in self.courses_attached
+                      and course in lecturer.courses_in_progress):
+
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -23,10 +26,10 @@ class Student:
                     count += item
                 return round(count/countlen, 2)
 
-        return f"Имя: {self.name}\n"f"Фамилия: {self.surname}\n"
-            f"Средняя оценка за домашние задания: {self.rate_Mentor()}\n"
-            f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
-            f"Завершенные курсы: {', '.join(self.courses_attached)}"
+        return (f"Имя: {self.name}\n"f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за домашние задания: {self.rate_Mentor()}\n"
+                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)}\n"
+                f"Завершенные курсы: {', '.join(self.courses_attached)}")
 
 
 class Mentor:
@@ -50,12 +53,16 @@ class Lecturer(Mentor):
             return round(count/countlen, 2)
 
     def __str__(self):
-        return f"Имя: {self.name}\n"f"Фамилия: {self.surname}\n"f"Средняя оценка за лекции: {self.Lecturer()}"
+        return (f"Имя: {self.name}\n"
+                f"Фамилия: {self.surname}\n"
+                f"Средняя оценка за лекции: {self.Lecturer()}")
 
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance((student, Student)
+                      and course in self.courses_attached
+                      and course in student.courses_in_progress):
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
